bd46e299c13559ff5d0e83506e1b30b6836f0654
Sam-Nazarov <screemer73@gmail.com>
2025-03-01 11:22:26 +0100

    лекция 3, добавил js файл

---
+++ JS-Course/Lection 3/lection.js
@@ -0,0 +1,194 @@
+//"use strict";
+
+// let myVariable;
+
+// myVariable = 52;
+
+// console.log(myVariable);
+// myVariable = 10;
+
+//ctrl + A + K + F
+
+// let numberOfStudents = 52;
+
+// console.log(numberOfStudents);
+
+// let a, b, c;
+
+// a = 10;
+// b = 20;
+// c = a + b;
+// console.log(a, b, c);
+
+// let a = 10,
+//   b = 20,
+//   c = 30;
+// console.log(a, b, c);
+
+// let student = "Tatiana";
+
+// console.log(student);
+
+// const student = "Tatiana";
+// student = "Oleg";
+// console.log(student);
+
+// let numberOfStudents = 52;
+
+// let _ = 1;
+// let $ = 2;
+
+// let a = 10;
+// let A = 20;
+// console.log(a, A);
+
+// let throw =
+
+// a = 10;
+
+// let student_name = "Bogdan";
+
+// const numberOfStudents = 52;
+
+// const PORT = 3000;
+
+/*
+//Примитивы
+string = 'asdasd' "asdsada" `adsda`
+number = 1232131.123123
+boolean = true, false
+null
+undefined
+symbol
+bigInt = 1234567890123456789012345678901234567890n
+//Ссылочные
+object = { name: 'Anna', age: 18 }
+*/
+
+// const variable = 1;
+
+// console.log(typeof variable);
+
+//Преобразование типов
+//to string
+// let numberValue = 1;
+// let booleanValue = true;
+// let nullValue = null;
+// let undefinedValue = undefined;
+// // const result = String(value);
+// const result = "" + undefinedValue;
+// console.log(typeof result);
+// console.log(result);
+// let numberValue = 1;
+// numberValue = String(numberValue);
+// console.log(typeof numberValue);
+
+//to number
+
+// let stringValue = "1.6";
+
+// const variant1 = Number(stringValue);
+// const variant2 = parseInt(stringValue);
+// const variant3 = parseFloat(stringValue);
+// const variant4 = +stringValue;
+
+// console.log(typeof variant1);
+// console.log(variant1);
+// console.log(typeof variant2);
+// console.log(variant2);
+// console.log(typeof variant3);
+// console.log(variant3);
+// console.log(typeof variant4);
+// console.log(variant4);
+
+// let stringValue = "1.6a";
+
+// const result = Number(stringValue);
+// console.log(result);
+// console.log(typeof result);
+
+// console.log(Number(""));
+
+// При преобразовании в число получится:
+// undefined => NaN
+// null => 0
+// true => 1
+// false => 0
+// string => 0 (если строка пустая),
+//          число (если в строке только цифры),
+//          NaN если в строке есть НЕ цифры
+
+//to Boolean
+
+/*
+  Falsy значения:
+  - 0
+  - ""
+  - null
+  - undefined
+  - NaN
+  - false
+*/
+
+// console.log(Boolean("0"));
+
+//Math operators
+// let x = "1";
+// x = -x;
+// console.log(typeof x);
+// let x = 1;
+// let y = 2;
+// console.log(x + y);
+
+// let str1 = "abc";
+// let str2 = "def";
+// console.log(str1 + str2);
+
+// console.log(x + str1);
+
+// console.log(1 + 1 + "1");
+
+// const result = 1 + 1 + "1";
+
+// console.log("1" - 1);
+
+// const result = 5 % 3;
+// console.log(result);
+
+// const result = "5" ** 3;
+// console.log(result);
+
+// console.log("b" + "a" + +"a" + "a");
+
+// let value = 1;
+
+// value = value + 1;
+// value += 1;
+// value -= 1;
+
+// console.log(value);
+
+//increment decrement
+
+// let value = 1;
+
+// value++;
+// value--;
+
+// console.log(value);
+/*
+
+let value1 = 1;
+let value2 = 1;
+
+let incrementedValue1 = value1++;
+let incrementedValue2 = ++value2;
+
+console.log(incrementedValue1);
+console.log(incrementedValue2);
+
+console.log(value1);
+console.log(value2);
+
+incrementedValue2 = ++value2;
+console.log(incrementedValue2);*/
